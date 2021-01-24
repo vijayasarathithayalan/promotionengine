94bd392dff4bfe9690b8cef6a0396cd49a31a421
Vijaya Sarathi <support@vysar.com>
2021-01-24 12:55:30 +0530

    Unit Test Project added

---
+++ UnitTesting/Test.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
